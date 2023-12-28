9a6c1702e47ca26d1b5bd3ad54927570a2b215ec
aalansloz <101714370+aalansloz@users.noreply.github.com>
2023-12-28 10:42:59 +0100

    minor update

---
+++ assets/js/adds.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+  atOptions = {
+    'key' : 'a626d552c36287ee3834f877792f5f0d',
+    'format' : 'iframe',
+    'height' : 90,
+    'width' : 728,
+    'params' : {}
+  };
+  document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/a626d552c36287ee3834f877792f5f0d/invoke.js"></scr' + 'ipt>');
+</script>
